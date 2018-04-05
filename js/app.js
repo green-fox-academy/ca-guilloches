0f727225591a4fce2829d57532d80b592f3519c5
Szász Tibor <tszasz@gmail.com>
2018-04-05 12:59:53 +0200

    feat(canvas): drawing test

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 'use strict';
 
-console.log('ok')
+function testCanvas() {
+    const canvas = document.querySelector('canvas');
+    const ctx = canvas.getContext('2d');
+    ctx.fillRect(10,10,300,400);
+}
+
+testCanvas()
