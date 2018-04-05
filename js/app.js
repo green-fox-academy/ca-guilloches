8e1257bab295cd23c68ea88c2f04fc8a0c4f53a9
Szász Tibor <tszasz@gmail.com>
2018-04-05 13:28:28 +0200

    feat(gulloches): crude draw function works

--- js/app.js
+++ js/app.js
@@ -1,9 +1,46 @@
 'use strict';
 
+const majorRipple = 57;
+const minorRipple = 0.13;
+const radiusEffect = 25;
+const angleMultiplier = 1.25;
+const amplitude = 2.5;
+
 function testCanvas() {
+    const startAt = {x:0,y:0};
     const canvas = document.querySelector('canvas');
     const ctx = canvas.getContext('2d');
-    ctx.fillRect(10,10,300,400);
+    drawLineCircle(ctx, 100);
+    ctx.stroke();
+}
+
+function drawLine(ctx, cx, cy, x, y) {
+    let offset = 300;
+    ctx.moveTo(offset + cx, offset + cy);
+    ctx.lineTo(offset + x, offset + y);
+}
+
+function drawLineCircle(ctx, numSegments=60) {
+    let theta = 0,
+    rp = minorRipple + radiusEffect,
+    s = (majorRipple + minorRipple) / minorRipple,
+    rR = majorRipple + minorRipple,
+    x = 0,
+    y = 0,
+    px = 0,
+    py = 0,
+    thetaStep = 2*Math.PI / numSegments;
+
+    for (let i = 0; i < numSegments; i++) {
+        x = rR * Math.cos(angleMultiplier * theta) + rp * Math.cos(s * angleMultiplier * theta);
+        y = rR * Math.sin(angleMultiplier * theta) + rp * Math.sin(s * angleMultiplier * theta);
+        theta += thetaStep;
+        x *= amplitude;
+        y *= amplitude;
+        drawLine(ctx, px, py, x, y);
+        px = x;
+        py = y;
+    }
 }
 
 testCanvas()
