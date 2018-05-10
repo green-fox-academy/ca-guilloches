908f67fe814b697409d7ed607e861efed9c3cb7d
Szász Tibor <tszasz@gmail.com>
2018-05-10 13:47:55 +0200

    feat(app): guilloches plotting works with control value

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ class App extends Component {
 	constructor() {
 		super();
 		this.ctx = null;
+
+		this.options = {
+			majorRipple: 50,
+			minorRipple: 0.15,
+			radiusEffect:25,
+			angleMultiplier: 1.9,
+			scale: 2.5,
+			numSegments: 3000
+		}
 	}
 
 	componentDidMount() {
@@ -14,10 +23,42 @@ class App extends Component {
 
 	updateCanvas(pos) {
 		this.clearCanvas();
-		this.drawLine(0, 0, pos, pos);
-		this.ctx.fillRect(pos, pos, 100, 100);
+		this.options.majorRipple = pos;
+		this.plotGuilloches(this.options);
+	}
+
+	plotGuilloches(options) {
+		const {minorRipple, majorRipple, radiusEffect, angleMultiplier, scale, numSegments} = options;
+
+		this.ctx.beginPath();
+		let theta = 0,
+		rp = minorRipple + radiusEffect,
+		s = (majorRipple + minorRipple) / minorRipple,
+		rR = majorRipple + minorRipple,
+		x,
+		y,
+		px,
+		py,
+		thetaStep = 2*Math.PI / numSegments;
+
+		let initPos = this.getPlotCoordinates(rR, angleMultiplier, theta, rp, s);
+		x = initPos.x * scale;
+		y = initPos.y * scale;
+		px = x;
+		py = y;
+
+		for (let i = 0; i < numSegments; i++) {
+			theta += thetaStep;
+			let coords = this.getPlotCoordinates(rR, angleMultiplier, theta, rp, s);
+			x = coords.x * scale;
+			y = coords.y * scale;
+			this.drawLine(px, py, x, y);
+			px = x;
+			py = y;
+		}
 		this.ctx.stroke();
 	}
+
 
 	drawLine(cx, cy, x, y) {
 		const offsetX = this.refs.canvas.width / 2;
@@ -25,6 +66,13 @@ class App extends Component {
 		this.ctx.moveTo(offsetX + cx, offsetY + cy);
 		this.ctx.lineTo(offsetX + x, offsetY + y);
 	}
+
+	getPlotCoordinates(rR, angleMultiplier, theta, rp, s) {
+		const x = rR * Math.cos(angleMultiplier * theta) + rp * Math.cos(s * angleMultiplier * theta);
+		const y = rR * Math.sin(angleMultiplier * theta) + rp * Math.sin(s * angleMultiplier * theta);
+		return {x, y};
+	}
+
 
 	clearCanvas() {
 		this.ctx.clearRect(
