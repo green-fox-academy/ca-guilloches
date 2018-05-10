110a54071e3f7e4a7e67697f4759921562623f99
Szász Tibor <tszasz@gmail.com>
2018-05-10 13:19:38 +0200

    feat(control): emit values by a callback

--- src/App.js
+++ src/App.js
@@ -2,23 +2,27 @@ import React, { Component } from 'react';
 import Control from './components/Control'
 
 class App extends Component {
-  componentDidMount() {
-    this.updateCanvas();
-  }
+	componentDidMount() {
+		this.updateCanvas();
+	}
 
-  updateCanvas() {
-      const ctx = this.refs.canvas.getContext('2d');
-      ctx.fillRect(0,0, 100, 100);
-  }
+	updateCanvas() {
+		const ctx = this.refs.canvas.getContext('2d');
+		ctx.fillRect(0,0, 100, 100);
+	}
 
-  render() {
-    return (
-      <div className="App">
-        <canvas ref="canvas" width={600} height={300}/>
-        <Control name="Size" default="1" min="0" max="200" />
-      </div>
-    );
-  }
+	updateProperty( value ) {
+		console.log("value from control", value);
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<canvas ref="canvas" width={600} height={300}/>
+				<Control name="Size" default="1" min="0" max="200" callback={ this.updateProperty } />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/Control.js
+++ src/components/Control.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Control extends Component {
 
 	changeEvent(e) {
-		console.log(this.refs.slider.value)
+		console.log(this.props.callback(this.refs.slider.value))
 	}
 
 	render() {
